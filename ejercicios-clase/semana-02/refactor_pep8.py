42726fa8e2ccdf989bc60011f8bd37c73ffb505e
juanf33 <juanfelipecalderon5@gmail.com>
2026-08-16 23:52:58 -0500

    Refactoriza refactor_pep8.py

--- ejercicios-clase/semana-02/refactor_pep8.py
+++ ejercicios-clase/semana-02/refactor_pep8.py
@@ -1,8 +1,24 @@
-def CalcularPromedio(Lista):
-    s=0
-    for x in Lista:
-     s=s+x
-    return s/len(Lista)
- 
-l=[1,2,3,4,5]
-print(CalcularPromedio(l))
+def calcular_promedio(numeros: list[int]) -> float:
+    """Calcula el promedio de una lista de numeros.
+
+    Args:
+        numeros: lista de numeros (enteros o flotantes) a promediar.
+
+    Returns:
+        El promedio de los numeros como valor flotante.
+    """
+    suma = 0
+    for numero in numeros:
+        suma = suma + numero
+    return suma / len(numeros)
+
+
+def main() -> None:
+    """Punto de entrada del script."""
+    lista_ejemplo = [1, 2, 3, 4, 5]
+    promedio = calcular_promedio(lista_ejemplo)
+    print(promedio)
+
+
+if __name__ == "__main__":
+    main()
